ea2a90b5072e350b47936355282c4e9266b6e8d8
Zaric <z.zuoqiang@gmail.com>
2013-01-07 22:55:47 +0800

    update book content

--- booklib/src/main/resources/com/book/controllers/LoginRequired.java
+++ booklib/src/main/resources/com/book/controllers/LoginRequired.java
@@ -1,6 +1,11 @@
 package com.book.controllers;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 //这是一个annotation，所谓annotation就是一个“标签”，他的职责是“表明”
 //至于表明之后该怎么样？则由其他代码来处理
